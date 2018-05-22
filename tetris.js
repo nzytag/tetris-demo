992f892205e5b866ddb90114caf7eea7cc91a4bb
Hector Norza <hnorza@Hectors-MBP.localdomain>
2018-05-21 19:07:26 -0700

    added tree

--- tetris.js
+++ tetris.js
@@ -11,10 +11,33 @@ const matrix = [
     [1, 1, 1],
     [0, 1, 0],
 ];
+
+function collide(arena, player) {
+    const [m, o] = [player.matrix, player.pos];
+    for (let y = 0; y < m.length; ++y) {
+        for (let x = 0; x < m[y].length; ++x) {
+            if(m[y][x]!== 0 &&
+            (arena[y + o.y]&&
+            arena [y + o.y][x + o.x]) !== 0) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function createMatrix(w,h){
+    const matrix =[];
+    while (h--){
+        matrix.push(new Array(w).fill(0));
+    }
+    return matrix;
+}
+
 function draw() {
     context.fillStyle ='#000';
     context.fillRect(0,0, canvas.width, canvas.height);
-
+drawMatrix(arena, {x:0, y:0})
     drawMatrix(player.matrix, player.pos);
 }
 
@@ -31,6 +54,32 @@ matrix.forEach((row, y) => {
     });
 }
 
+function merge(arena, player) {
+    player.matrix.forEach((row, y) => {
+        row.forEach((value, x)=> {
+            if(value !== 0){
+                arena[y + player.pos.y][x+player.pos.x] = value;
+            }
+        });
+    });
+}
+function playerDrop() {
+    player.pos.y++;
+    if(collide(arena, player)){
+        player.pos.y--;
+        merge(arena, player);
+        player.pos.y =0;
+    }
+    dropCounter = 0;
+}
+
+function playerMove(dir){
+    player.pos.x +=dir;
+    if(collide(arena,player)){
+        player.pos.x-=dir;
+    }
+}
+
 let dropCounter = 0;
 let dropInterval = 1000;
 
@@ -42,18 +91,28 @@ function update(time = 0){
 
     dropCounter+= deltaTime;
     if (dropCounter > dropInterval) {
-        player.pos.y++;
-        dropCounter =0;
+        playerDrop();
     }
     draw();
     requestAnimationFrame(update);
 }
 
 
+const arena = createMatrix(12, 20);
+
 const player = {
     pos: {x: 5, y: 5},
     matrix: matrix,
-
+}
+document.addEventListener('keydown', event => {
+if (event.keyCode === 37) {
+    playerMove(-1);
+} else if (event.keyCode === 39){
+    playerMove(1);
+} else if (event.keyCode ===40){
+    playerDrop();
 }
 
+
+});
 update();
